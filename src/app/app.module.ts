23382602050eb96f750fc6ed8a86c497b44af000
rouscast <rociocy@gmail.com>
2019-08-25 21:05:37 -0300

    ddd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 //Rutas
 import { APP_ROUTES } from './app.routes';
 //Modulos
-
+//temporal
+import { FormsModule } from '@angular/forms';
 import { PagesModule } from './pages/pages.module';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
@@ -11,18 +12,23 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+  
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     APP_ROUTES,
     PagesModule,
-    SharedModule
+    SharedModule,
+    FormsModule,
 
   ],
   providers: [],
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { PAGES_ROUTES } from './pages.routes';
 
 import { ProgressComponent } from './progress/progress.component';
@@ -6,6 +7,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
+//charts
+import { ChartsModule } from 'ng2-charts';
+
+//temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
+
 @NgModule({
     declarations: [
 
@@ -13,6 +21,8 @@ import { SharedModule } from '../shared/shared.module';
         ProgressComponent,
         Graficas1Component,
         PagesComponent,
+        IncrementadorComponent,
+        GraficoDonaComponent
     ],
     exports: [
         DashboardComponent,
@@ -20,10 +30,14 @@ import { SharedModule } from '../shared/shared.module';
         Graficas1Component,
         PagesComponent,
 
+
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule,
+        ChartsModule
+
     ]
 })
 
